70d26f5595c31a1d092f92f7135d770cf62009ea
sub0ne1 <iamsubhajit.d@gmail.com>
2023-07-18 22:53:37 -0400

    few more changes

---
+++ src/components/AddTaskButton.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function AddTaskButton(){
+  return(
+    <View>
+      <Text>Add Task</Text>
+    </View>
+  )
+}
+const addTaskButtonStyles = StyleSheet.create({
+  
+})
