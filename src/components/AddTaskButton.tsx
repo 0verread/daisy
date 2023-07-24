d8e8895e10c923dcced10a3d159b222e3a015856
sub0ne1 <iamsubhajit.d@gmail.com>
2023-07-23 23:05:37 -0400

    handle add task button

--- src/components/AddTaskButton.tsx
+++ src/components/AddTaskButton.tsx
@@ -1,22 +1,42 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, TouchableOpacity } from "react-native";
+
+const handleAddTaskButton = () => {
+  
+}
 
 export default function AddTaskButton(){
   return(
-    <View style={addTaskButtonStyles.container}>
-      <Button title="add"/>
-    </View>
+      <View style={addTaskButtonStyles.container}>
+        <TouchableOpacity onPress={() => handleAddTaskButton()}>
+          <View style={addTaskButtonStyles.addTaskWrapper}>
+            <Text style={addTaskButtonStyles.addTaskText}>Add a task</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
   )
 }
 const addTaskButtonStyles = StyleSheet.create({
  container: {
-  // flexDirection: "row",
   position: 'absolute',
   bottom: 60,
   width: '100%',
   flexDirection: 'row',
   justifyContent: 'space-around',
   alignItems: 'center'
-
- } 
+ },
+ addTaskWrapper: {
+  paddingVertical: 15,
+  paddingHorizontal: 15,
+  backgroundColor: '#FFF',
+  borderRadius: 60,
+  borderColor: '#C0C0C0',
+  borderWidth: 1,
+  width: 240,
+  alignItems: "center"
+ },
+ addTaskText: {
+  fontSize: 16,
+  fontWeight: "600"
+ }
 })
